c8203ec8f2c36dc07c82cec0dcf480d506f71201
wormholecowboy <wormholecowboy@gmail.com>
2023-10-31 12:13:20 -0400

    adjusted context and illuminate

--- .config/nvim/lua/wormholecowboy/plugins/nvim-treesitter-context.lua
+++ .config/nvim/lua/wormholecowboy/plugins/nvim-treesitter-context.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require("treesitter-context").setup({
 			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+			max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
 			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 			line_numbers = true,
 			multiline_threshold = 20, -- Maximum number of lines to show for a single context
@@ -13,9 +13,15 @@ return {
 			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
 			-- Separator between context and content. Should be a single character string, like '-'.
 			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-			separator = "-",
+			separator = "",
 			zindex = 20, -- The Z-index of the context window
 			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 		})
 	end,
+	vim.keymap.set("n", "[c", function()
+		require("treesitter-context").go_to_context()
+	end, { silent = true }),
+	vim.cmd("hi TreesitterContextBottom gui=underline guisp=Grey"),
+	vim.cmd("hi TreesitterContextLineNumber gui=underline guisp=Grey"),
+	vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { link = "Search" }),
 }
