a47fdb342f2a08db2f2e2d8639bffc1c2b665fab
wormholecowboy <wormholecowboy@gmail.com>
2024-01-08 17:28:29 -0500

    gitconfig

--- .config/nvim/lua/wormholecowboy/core/init.lua
+++ .config/nvim/lua/wormholecowboy/core/init.lua
@@ -1,2 +1,8 @@
 require('wormholecowboy.core.keymaps')
 require('wormholecowboy.core.options')
+
+vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
