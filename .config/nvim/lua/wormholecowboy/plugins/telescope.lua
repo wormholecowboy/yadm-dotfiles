33753d51461efd159d349b015b8e17b734f90cc8
wormholecowboy <wormholecowboy@gmail.com>
2024-04-02 13:52:07 -0400

    Ripped out lsp zero. Added which key.

--- .config/nvim/lua/wormholecowboy/plugins/telescope.lua
+++ .config/nvim/lua/wormholecowboy/plugins/telescope.lua
@@ -1,53 +1,60 @@
 return {
 
-    "nvim-telescope/telescope.nvim",
-    version = "0.1.2",
-    dependencies = { { "nvim-lua/plenary.nvim" } },
-    config = function()
-        local status_ok, telescope = pcall(require, "telescope")
-        if not status_ok then
-            return
-        end
+  "nvim-telescope/telescope.nvim",
+  version = "0.1.2",
+  dependencies = { { "nvim-lua/plenary.nvim" } },
+  config = function()
+    local status_ok, telescope = pcall(require, "telescope")
+    if not status_ok then
+      return
+    end
 
-        local actions = require("telescope.actions")
-        local builtin = require("telescope.builtin")
+    local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
 
-        vim.keymap.set("n", "<leader>sgf", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>sgb", builtin.git_branches, {})
-        vim.keymap.set("n", "<leader>sgc", builtin.git_commits, {})
-        vim.keymap.set("n", "<leader>sgs", builtin.git_status, {})
-        vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>sb", builtin.current_buffer_fuzzy_find, {})
-        vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
-        vim.keymap.set("n", "<leader>scs", builtin.colorscheme, {})
-        vim.keymap.set("n", "<leader>scc", builtin.commands, {})
-        vim.keymap.set("n", "<leader>sch", builtin.command_history, {})
-        vim.keymap.set("n", "<leader>sr", builtin.oldfiles, {})
-        vim.keymap.set("n", "<leader>sk", builtin.keymaps, {})
-        vim.keymap.set("n", "<leader>sm", builtin.marks, {})
-        vim.keymap.set("n", "<leader>sn", builtin.man_pages, {})
-        vim.keymap.set("n", "<leader>sa", builtin.autocommands, {})
-        vim.keymap.set("n", "<leader>ss", function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+    function searchString()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end
 
-        telescope.setup({
-            defaults = {
+    local wk = require("which-key")
+    wk.register({
+      s = {
+        s = { "<cmd>lua searchString()<cr>", "string" },
+        f = { "<cmd>Telescope find_files<cr>", "files" },
+        h = { "<cmd>Telescope help_tags<cr>", "help" },
+        b = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "buffer" },
+        cs = { "<cmd>Telescope colorscheme<cr>", "color scheme" },
+        cc = { "<cmd>Telescope commands<cr>", "commands" },
+        ch = { "<cmd>Telescope command_history<cr>", "command_history" },
+        r = { "<cmd>Telescope oldfiles<cr>", "recent" },
+        k = { "<cmd>Telescope keymaps<cr>", "keymaps" },
+        m = { "<cmd>Telescope marks<cr>", "marks" },
+        n = { "<cmd>Telescope man_pages<cr>", "man pages" },
+        a = { "<cmd>Telescope autocommands<cr>", "man pages" },
+        gf = { "<cmd>Telescope git_files<cr>", "git files" },
+        gb = { "<cmd>Telescope git_branches<cr>", "git branches" },
+        gc = { "<cmd>Telescope git_commits<cr>", "git commits" },
+        gs = { "<cmd>Telescope git_status<cr>", "git status" },
+      },
+    }, { prefix = "<leader>" })
 
-                prompt_prefix = " ",
-                selection_caret = " ",
-                path_display = { "smart" },
-                file_ignore_patterns = { "node_modules" },
+    telescope.setup({
+      defaults = {
 
-                mappings = {
-                    i = {
-                        ["<Down>"] = actions.cycle_history_next,
-                        ["<Up>"] = actions.cycle_history_prev,
-                        ["<C-j>"] = actions.move_selection_next,
-                        ["<C-k>"] = actions.move_selection_previous,
-                    },
-                },
-            },
-        })
-    end,
+        prompt_prefix = " ",
+        selection_caret = " ",
+        path_display = { "smart" },
+        file_ignore_patterns = { "node_modules" },
+
+        mappings = {
+          i = {
+            ["<Down>"] = actions.cycle_history_next,
+            ["<Up>"] = actions.cycle_history_prev,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+        },
+      },
+    })
+  end,
 }
