8039226a1e5849839e5ec1b351412d7b10c428fd
wormholecowboy <wormholecowboy@gmail.com>
2024-05-15 17:51:31 -0400

    bufferline, zen mode, color changes. removed twilight.

--- .config/nvim/lua/wormholecowboy/plugins/colors.lua
+++ .config/nvim/lua/wormholecowboy/plugins/colors.lua
@@ -12,7 +12,7 @@ return {
 	},
 	config = function()
 		function ColorMyPencils(color)
-			color = color or "slate"
+			color = color or "noctis"
 			vim.cmd.colorscheme(color)
 
 			-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
