213459e1af7dbad978986336d7caafa81d41fabc
wormholecowboy <wormholecowboy@gmail.com>
2024-05-24 18:06:45 -0400

    cmp snippets

--- .config/nvim/lua/wormholecowboy/plugins/cmp.lua
+++ .config/nvim/lua/wormholecowboy/plugins/cmp.lua
@@ -5,10 +5,10 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "rafamadriz/friendly-snippets",
-    "L3MON4D3/LuaSnip",
+    { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
     "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind.nvim",
-    "hrsh7th/cmp-cmdline",
+    -- "hrsh7th/cmp-cmdline",
     { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
   },
   config = function()
@@ -18,21 +18,24 @@ return {
     local tailwind_formatter = require("tailwindcss-colorizer-cmp").formatter
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    -- require("luasnip").log.set_loglevel("info")
     require("luasnip.loaders.from_vscode").lazy_load()
 
-    local check_backspace = function() -- from chris@machine (needed for supertab)
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip", option = { use_show_condition = false } },
+        -- { name = "luasnip" },
+        { name = "buffer", keyword_length = 3 },
+        { name = "path" },
+      }),
       completion = {
         completeopt = "menu,menuone,preview,noinsert",
-      },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
       },
       window = {
         completion = {
@@ -43,78 +46,37 @@ return {
           scrolloff = 8,
         },
         documentation = {
-          -- border = "none",
           winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
         },
       },
-      mapping = cmp.mapping.preset.insert({
-
-        ["<C-l>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-        ["<C-k>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-j>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        ["<Tab>"] = cmp.mapping(function(fallback) -- SuperTAB stuff
-          --[[ if cmp.visible() then
-						cmp.select_next_item() ]]
-          if luasnip.jumpable(1) then
-            luasnip.jump(1)
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif luasnip.expandable() then
-            luasnip.expand()
-          elseif check_backspace() then
-            -- cmp.complete()
-            fallback()
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          --[[ if cmp.visible() then
-						cmp.select_prev_item() ]]
-          if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-      }),
-
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer", keyword_length = 3 },
-        { name = "path" },
-      }),
-
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-          menu = {
-            nvim_lsp = "lsp",
-            nvim_lua = "lua",
-            path = "path",
-            buffer = "buf",
-            luasnip = "snip",
-            treesitter = "",
-            zsh = "",
-          },
-          before = tailwind_formatter,
-        }),
+      mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-l>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-y>"] = cmp.mapping(
+          cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          }),
+          { "i", "c" }
+        ),
       },
+      -- formatting = {
+      --   format = lspkind.cmp_format({
+      --     maxwidth = 50,
+      --     ellipsis_char = "...",
+      --     menu = {
+      --       nvim_lsp = "lsp",
+      --       nvim_lua = "lua",
+      --       path = "path",
+      --       buffer = "buf",
+      --       luasnip = "snip",
+      --       treesitter = "",
+      --       zsh = "",
+      --     },
+      --     before = tailwind_formatter,
+      --   }),
+      -- },
     })
   end,
 }
