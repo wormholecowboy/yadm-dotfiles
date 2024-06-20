25b47b1a86d608a6b7fb000b0346a46670c604e6
wormholecowboy <wormholecowboy@gmail.com>
2024-06-20 14:21:24 -0400

    aerial

---
+++ .config/nvim/lua/wormholecowboy/plugins/aerial.lua
@@ -0,0 +1,33 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("aerial").setup({
+      backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
+
+      layout = {
+        -- These control the width of the aerial window.
+        -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        -- min_width and max_width can be a list of mixed types.
+        -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+        max_width = { 40, 0.2 },
+        width = nil,
+        min_width = 10,
+      },
+      lazy_load = true,
+      close_on_select = true,
+      autojump = true,
+      highlight_on_hover = true,
+      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      on_attach = function(bufnr)
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    })
+    vim.keymap.set("n", "<leader>A", "<cmd>AerialToggle!<CR>")
+  end,
+}
